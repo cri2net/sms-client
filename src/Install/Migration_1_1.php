4607925f04d24a8a88089b11251b49e49d21569a
Andrey Davidovich <cri2net@gmail.com>
2021-08-10 00:29:24 +0300

    fix for error "There is no active transaction" in php 8

--- src/Install/Migration_1_1.php
+++ src/Install/Migration_1_1.php
@@ -2,7 +2,6 @@
 
 namespace cri2net\sms_client\Install;
 
-use Exception;
 use cri2net\php_pdo_db\PDO_DB;
 use Placebook\Framework\Core\Install\MigrationInterface;
 
@@ -11,72 +10,60 @@ class Migration_1_1 implements MigrationInterface
     public static function up()
     {
         $prefix = (defined('TABLE_PREFIX')) ? TABLE_PREFIX : '';
-        $pdo = PDO_DB::getPDO();
 
-        try {
-            $pdo->beginTransaction();
+        switch (PDO_DB::getParams('type')) {
+            case 'pgsql':
+                PDO_DB::query("CREATE TYPE sms_status_enum AS ENUM ('new', 'sending', 'complete', 'cancel', 'fail');");
+
+                PDO_DB::query("CREATE SEQUENCE {$prefix}sms_seq;");
+                PDO_DB::query(
+                    "CREATE TABLE {$prefix}sms (
+                        id int NOT NULL DEFAULT NEXTVAL ('{$prefix}sms_seq'),
+                        status sms_status_enum NOT NULL DEFAULT 'new',
+                        \"to\" character varying(100) NOT NULL,
+                        created_at double precision NOT NULL,
+                        updated_at double precision NOT NULL,
+                        send_at double precision,
+                        min_sending_time double precision NOT NULL,
+                        replace_data text,
+                        raw_text text,
+                        processing character varying(50),
+                        processing_data text,
+                        processing_status character varying(50),
+                        additional text,
+                        PRIMARY KEY (id)
+                    )
+                    WITH (
+                        OIDS = FALSE
+                    );"
+                );
+                PDO_DB::query("ALTER SEQUENCE {$prefix}sms_seq RESTART WITH 1;");
+                PDO_DB::query("CREATE INDEX {$prefix}sms_status_index ON {$prefix}sms (status, min_sending_time);");
+                PDO_DB::query("CREATE INDEX {$prefix}sms_processing_index ON {$prefix}sms (processing);");
+                break;
             
-            switch (PDO_DB::getParams('type')) {
-                case 'pgsql':
-
-                    PDO_DB::query("CREATE TYPE sms_status_enum AS ENUM ('new', 'sending', 'complete', 'cancel', 'fail');");
-
-                    PDO_DB::query("CREATE SEQUENCE {$prefix}sms_seq;");
-                    PDO_DB::query(
-                        "CREATE TABLE {$prefix}sms (
-                            id int NOT NULL DEFAULT NEXTVAL ('{$prefix}sms_seq'),
-                            status sms_status_enum NOT NULL DEFAULT 'new',
-                            \"to\" character varying(100) NOT NULL,
-                            created_at double precision NOT NULL,
-                            updated_at double precision NOT NULL,
-                            send_at double precision,
-                            min_sending_time double precision NOT NULL,
-                            replace_data text,
-                            raw_text text,
-                            processing character varying(50),
-                            processing_data text,
-                            processing_status character varying(50),
-                            additional text,
-                            PRIMARY KEY (id)
-                        )
-                        WITH (
-                            OIDS = FALSE
-                        );"
-                    );
-                    PDO_DB::query("ALTER SEQUENCE {$prefix}sms_seq RESTART WITH 1;");
-                    PDO_DB::query("CREATE INDEX {$prefix}sms_status_index ON {$prefix}sms (status, min_sending_time);");
-                    PDO_DB::query("CREATE INDEX {$prefix}sms_processing_index ON {$prefix}sms (processing);");
-                    break;
-                
-                case 'mysql':
-                default:
-                    $pdo->query(
-                        "CREATE TABLE IF NOT EXISTS {$prefix}sms (
-                            id int(11) NOT NULL AUTO_INCREMENT,
-                            `status` enum('new','sending','complete','cancel','fail') NOT NULL DEFAULT 'new',
-                            `to` varchar(100) NOT NULL,
-                            created_at double NOT NULL,
-                            updated_at double NOT NULL,
-                            send_at double DEFAULT NULL,
-                            min_sending_time double NOT NULL,
-                            replace_data text,
-                            raw_text text,
-                            processing varchar(50) DEFAULT NULL,
-                            processing_data mediumtext,
-                            processing_status varchar(50) DEFAULT NULL,
-                            additional MEDIUMTEXT DEFAULT NULL,
-                            PRIMARY KEY (id),
-                            KEY `status` (`status`,min_sending_time),
-                            KEY processing (processing)
-                        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
-                    );
-            }
-
-            $pdo->commit();
-
-        } catch (Exception $e) {
-            $pdo->rollBack();
-            throw $e;
+            case 'mysql':
+            default:
+                PDO_DB::query(
+                    "CREATE TABLE IF NOT EXISTS {$prefix}sms (
+                        id int(11) NOT NULL AUTO_INCREMENT,
+                        `status` enum('new','sending','complete','cancel','fail') NOT NULL DEFAULT 'new',
+                        `to` varchar(100) NOT NULL,
+                        created_at double NOT NULL,
+                        updated_at double NOT NULL,
+                        send_at double DEFAULT NULL,
+                        min_sending_time double NOT NULL,
+                        replace_data text,
+                        raw_text text,
+                        processing varchar(50) DEFAULT NULL,
+                        processing_data mediumtext,
+                        processing_status varchar(50) DEFAULT NULL,
+                        additional MEDIUMTEXT DEFAULT NULL,
+                        PRIMARY KEY (id),
+                        KEY `status` (`status`,min_sending_time),
+                        KEY processing (processing)
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+                );
         }
     }
 
@@ -84,29 +71,16 @@ class Migration_1_1 implements MigrationInterface
     {
         $prefix = (defined('TABLE_PREFIX')) ? TABLE_PREFIX : '';
         
-        $pdo = PDO_DB::getPDO();
-
-        try {
-            $pdo->beginTransaction();
-            
-            switch (PDO_DB::getParams('type')) {
-                case 'pgsql':
-
-                    PDO_DB::query("DROP TABLE IF EXISTS {$prefix}sms;");
-                    PDO_DB::query("DROP TYPE IF EXISTS sms_status_enum;");
-                    PDO_DB::query("DROP SEQUENCE IF EXISTS {$prefix}sms_seq;");
-                    break;
-                    
-                case 'mysql':
-                default:
-                    PDO_DB::query("DROP TABLE IF EXISTS {$prefix}sms");
-            }
-
-            $pdo->commit();
-
-        } catch (Exception $e) {
-            $pdo->rollBack();
-            throw $e;
+        switch (PDO_DB::getParams('type')) {
+            case 'pgsql':
+                PDO_DB::query("DROP TABLE IF EXISTS {$prefix}sms;");
+                PDO_DB::query("DROP TYPE IF EXISTS sms_status_enum;");
+                PDO_DB::query("DROP SEQUENCE IF EXISTS {$prefix}sms_seq;");
+                break;
+                
+            case 'mysql':
+            default:
+                PDO_DB::query("DROP TABLE IF EXISTS {$prefix}sms");
         }
     }
 }
--- src/Install/Migration_1_1_1.php
+++ src/Install/Migration_1_1_1.php
@@ -2,7 +2,6 @@
 
 namespace cri2net\sms_client\Install;
 
-use Exception;
 use cri2net\php_pdo_db\PDO_DB;
 use Placebook\Framework\Core\Install\MigrationInterface;
 
@@ -12,31 +11,20 @@ class Migration_1_1_1 implements MigrationInterface
     {
         $prefix = (defined('TABLE_PREFIX')) ? TABLE_PREFIX : '';
 
-        try {
-            switch (PDO_DB::getParams('type')) {
-                case 'pgsql':
-                    PDO_DB::query("ALTER TABLE {$prefix}sms ADD COLUMN alfaname character varying(50)");
-                    break;
+        switch (PDO_DB::getParams('type')) {
+            case 'pgsql':
+                PDO_DB::query("ALTER TABLE {$prefix}sms ADD COLUMN alfaname character varying(50)");
+                break;
 
-                case 'mysql':
-                default:
-                    PDO_DB::query("ALTER TABLE {$prefix}sms ADD COLUMN alfaname VARCHAR(50) NULL AFTER additional");
-            }
-
-        } catch (Exception $e) {
-            throw $e;
+            case 'mysql':
+            default:
+                PDO_DB::query("ALTER TABLE {$prefix}sms ADD COLUMN alfaname VARCHAR(50) NULL AFTER additional");
         }
     }
 
     public static function down()
     {
         $prefix = (defined('TABLE_PREFIX')) ? TABLE_PREFIX : '';
-
-        try {
-            PDO_DB::query("ALTER TABLE {$prefix}sms DROP COLUMN alfaname");
-
-        } catch (Exception $e) {
-            throw $e;
-        }
+        PDO_DB::query("ALTER TABLE {$prefix}sms DROP COLUMN alfaname");
     }
 }
